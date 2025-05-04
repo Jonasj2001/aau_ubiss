28dcae8073bd1ad46d3d08cb9b70acd6ac33bf85
Jonas Jakobsen <jojako21@student.aau.dk>
2025-05-04 22:55:03 +0100

    Updated MQTT

--- board_support_crate/aau_ubiss/_core.py
+++ board_support_crate/aau_ubiss/_core.py
@@ -1,8 +1,11 @@
+
 #%%
-import board
+import time
 from enum import Enum
 from typing import Literal
-from adafruit_bme680 import Adafruit_BME680_I2C as BME680, _BME680_SAMPLERATES
+import requests
+import board
+from adafruit_bme680 import Adafruit_BME680_I2C as BME680
 from adafruit_veml7700 import VEML7700
 from adafruit_sgp30 import Adafruit_SGP30 as SGP30
 import paho.mqtt.client as mqtt
@@ -61,6 +64,7 @@ class _pressure:
         self.sensor.pressure_oversample = rate.value
 
 class _light:
+    """Class for controlling Light parameters of the VEML7700 sensor"""
     class int_time_val(Enum):
         ALS_25MS = VEML7700.ALS_25MS
         ALS_50MS = VEML7700.ALS_50MS
@@ -75,7 +79,6 @@ class _light:
         ALS_GAIN_1_8 = VEML7700.ALS_GAIN_1_8
         ALS_GAIN_1_4 = VEML7700.ALS_GAIN_1_4
     
-    """Class for controlling Light parameters of the VEML7700 sensor"""
     def __init__(self, sensor: VEML7700):
         self.sensor = sensor
 
@@ -93,7 +96,7 @@ class _light:
     
     @integration_time.setter
     def integration_time(self, int_time: int_time_val):
-        if int_time not in self.int_time:
+        if int_time not in self.int_time_val:
             raise ValueError("Invalid value must be in int_time")
         self.sensor.light_integration_time = int_time.value
     
@@ -121,7 +124,7 @@ class _gas:
         """
         return [self.sensor.TVOC, self.sensor.eCO2]
     
-    def getBaseline(self):
+    def get_baseline(self):
         """Return Baseline values for TVOC and eCO2 in [ppb] and [ppm]
         
         Returns
@@ -130,15 +133,12 @@ class _gas:
         """
         return [self.sensor.baseline_TVOC, self.sensor.baseline_eCO2]
 
-
-
-
 class _messaging:
     def __init__(self, userid):
         self.userid = userid
 
     @staticmethod
-    def serialize(payload_list, timestamps, topic):
+    def _serialize(payload_list, timestamps, topic):
         string_out=str(topic)
         print(payload_list)
         for entry in payload_list:
@@ -150,32 +150,16 @@ class _messaging:
     
     @staticmethod
     def prepare_payload(sensors, data, timestamps):
-        """
-        #Specific requirements is that the "data", must be int or float,
-        # The timestamps must contain ":", and this should not be used in the sensors field.
-        # Wheter hour:minute:second is sent, or day/hour:minute:second, or minute:second, it will work
-        # if a ":" is present.
-        # The "sensors", must be a string, the name does not matter as it is more of an informative 
-        # parameter for you, so you could map "t" to temperature, but this is a matter for you to control.   
-        """
         if len(sensors)==1:
             payload = {"topic":[], "payload":[], "ts":[]}
             for s in sensors:
                 payload["topic"].append(s)
             for d in data:
-                flag = True
-                try:
-                # try converting to integer
-                    float(d)
-                except ValueError:
-                    flag = False
-                if flag==False:
-                    print("sensor value must be a number, int or float. No strings allowed")
-                    return -1
                 payload["payload"].append(d)
             for t in timestamps:
                 payload["ts"].append(t)
             return payload
+
 
         elif len(sensors)!= len(data) and len(sensors)!=len(timestamps):
             print("Must have equal set length of sensors, payload and timestamps")
@@ -187,13 +171,10 @@ class _messaging:
             payload["payload"].append(d)
         for t in timestamps:
             payload["ts"].append(t)
-        print(payload["ts"])
-        print(payload["payload"])
         return payload
         
     def send_topics(self, topic_payload) -> None:
         raise NotImplementedError()
-
 
 class messaging_ip(_messaging):
     @staticmethod
@@ -204,15 +185,13 @@ class messaging_ip(_messaging):
     def _on_message(client, userdata, msg):
         print(msg.topic+" "+str(msg.payload))
 
-    def __init__(self, server: str, userid="group", topic="/ubiss"):
+    def __init__(self, server: str, userid="group", topic="ubiss/"):
         super().__init__(userid)
         self.topic = topic
         self.client = mqtt.Client()
         self.client.on_connect = self._on_connect
         self.client.on_message = self._on_message
         self.client.connect(server)
-
-
     
     def send_topics(self, topic_payload) -> None:
         output = str(self.userid)
@@ -225,18 +204,18 @@ class messaging_ip(_messaging):
             payload= topic_payload['payload'][i]
             timestamp = topic_payload['ts'][i]
 
-            serialized_input = self.serialize(payload,timestamp,topic_payload['topic'][i])
+            serialized_input = self._serialize(payload,timestamp,topic_payload['topic'][i])
             output = output + "," + serialized_input
             
         self.client.publish(full_topic, output)
 
 
 class messaging_nbiot(_messaging):
-    def __init__(self):
-        super().__init__()
-
+    def __init__(self, userid):
+        super().__init__(userid)
 
 class aau_ubiss:
+    """AAU Ubiss, board support crate"""
     def __init__(self, server="172.20.0.22", userid="group"):
         bme = BME680(i2c, refresh_rate=100)
         bme.set_gas_heater(None, None)
@@ -249,6 +228,33 @@ class aau_ubiss:
         self.light = _light(veml7700)
         self.gas = _gas(sgp30)
         self.mqtt = messaging_ip(server, userid)
+        self._ip = server
+        self._uid = userid
+
+    def _fetch_file(self, server, port):
+        res = requests.get(
+            f"http://{server}:{port}/{self._uid}.csv", timeout=5
+            )
+        
+        with open(f"{self._uid}.csv", "w") as f:
+            f.write(res.text)
+
+    def download(self, localhost: bool = False) -> None:
+        """Fetch group data from the server"""
+        server = self._ip
+        port = 9080
+        if localhost:
+            server = "172.20.0.21"
+            port = 8080
+
+        topic = self.mqtt.topic+"download"
+        msg = self._uid+",all"
+        self.mqtt.client.publish(topic, msg) # Prepare download
+        time.sleep(2)
+        self._fetch_file(server, port)
+
+
+        
 
 #%%
 if __name__ == "__main__":
@@ -256,4 +262,3 @@ if __name__ == "__main__":
     print(ubiss.temperature())
     print(ubiss.pressure())
     print(ubiss.humidity())
-    ubiss.humidity.set_oversample_rate()

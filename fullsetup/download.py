23ca8ecc68ebb72c39e1db507fb07446b2e3fc06
Jonas Jakobsen <jonasj2001@hotmail.com>
2026-02-25 19:48:33 +0100

    Updated download without RPI dependancy

--- fullsetup/download.py
+++ fullsetup/download.py
@@ -1,15 +1,19 @@
+#!/bin/env python3
 import requests
 import paho.mqtt.client as mqtt
 import sys
-import util
 import os
 import time
 
-userid=util.userid
+mqtt_server = "130.225.37.202"
+
+fileserver = mqtt_server # Use 172.20.0.21 if locally hosted. 
+topic = "aauiot/"
+userid="group"
 data_to_download="all"
 
 def write_to_file(name, rawdata):
-	
+
     directory=os.getcwd()
     f= open(directory + "/" + name+".csv", 'w')
     data=rawdata.rsplit("\n")
@@ -18,33 +22,26 @@ def write_to_file(name, rawdata):
         f.write("\n")
 
 def download(userid):
-	server= util.server # if hosted remotely on a different # if #"130.225.57.224"
-	server="172.20.0.21" # If hosted locally on a computer via docker
-	#port="9080"
-	port="8080"
-	res = requests.get("http://"+server+":"+port+"/"+userid+".csv")
-	print(res.text)
-	write_to_file(userid, res.text)
+    port="9080"
+    if fileserver == "172.20.0.21":
+        port = "8080"
+    res = requests.get("http://"+fileserver+":"+port+"/"+userid+".csv")
+    print(res.text)
+    write_to_file(userid, res.text)
 
 
 
 
 client = mqtt.Client()
-client.connect(util.server,1883,60)
-client.on_connect = util.on_connect
-client.on_message = util.on_message
-
-
-
-
+client.connect(mqtt_server,1883,60)
 
 if len(sys.argv)>1:
-	output=str(sys.argv[1])+","+data_to_download
-	client.publish(util.topic+"download",output) # identifier
-	download(sys.argv[1])
+    output=str(sys.argv[1])+","+data_to_download
+    client.publish(topic+"download",output) # identifier
+    download(sys.argv[1])
 	
 else:
-	output=str(userid)+","+data_to_download
-	client.publish(util.topic+"download",output) # identifier
-	time.sleep(2)
-	download(userid)
+    output=str(userid)+","+data_to_download
+    client.publish(topic+"download",output) # identifier
+    time.sleep(2)
+    download(userid)
